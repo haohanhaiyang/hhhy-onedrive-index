f2ea1952bbf4a328c56880a1b1059cb2003bf334
皓瀚海洋 <83904312+haohanhaiyang@users.noreply.github.com>
2022-06-21 14:49:55 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'zh-CN',
+    defaultLocale: 'en',
     locales: ['en', 'zh-CN']
   },
   localePath: path.resolve('public/locales'),
